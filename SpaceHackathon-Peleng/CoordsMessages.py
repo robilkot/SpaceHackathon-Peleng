f13a84c5eb51ea82e561148722c45cbbe548be65
robilkot <timur.robilko@gmail.com>
2024-11-18 11:47:50 +0300

    model

---
+++ SpaceHackathon-Peleng/CoordsMessages.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+'''
+Каждый кадр леша отправляет ObjDetectedMessage для каждой камеры где обнаружен объект
+Егор принимает мессаджи, трианглуирует координаты, формирует сообщение CoordinatesTrackedMessage
+и отправляет Тимуру для записи в итоговый док
+
+Если на какой-то камере объект пропал, то леша НЕ отправляет сообщение для этой камеры на этом кадре
+угадывание траектории пропавшего объекта сдлеаем позже на стороне леши 
+'''
+
+@dataclass
+class ObjDetectedMessage:
+    cam_id: int
+    x: int  # coordinates in image
+    y: int
+    w: int
+    h: int
+    t: float  # time in seconds at which the detection happened
+
+
+@dataclass
+class CoordinatesTrackedMessage:
+    x: float
+    y: float
+    z: float
+    t: float
+    dl_max: float  # error (may be none for now)
